e3e9196b19e9b37ad3f7b5ad6385e1a2cd0ede2c
Prashant <pk3076889@gmail.com>
2022-05-05 14:05:08 +0530

    Question 4

---
+++ 4.py
@@ -0,0 +1,10 @@
+ch = input("Please Enter Your Own Character : ")
+
+if(ch >= 'A' and ch <= 'Z'):
+    print("The Given Character ", ch, "is an Uppercase Alphabet") 
+
+elif(ch >= 'a' and ch <= 'z'):
+    print("The Given Character ", ch, "is a Lowercase Alphabet")
+    
+else:
+    print("The Given Character ", ch, "is Not a Lower or Uppercase Alphabet")
